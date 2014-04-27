8915ab9b55a07c3eb410965f219dec8a3f49fbf0
DigitalVerve <info@digitalverve.de>
2014-04-27 23:28:23 +0200

    command line commit

--- provider.js
+++ provider.js
@@ -5,5 +5,5 @@ window.kdnr = "20004";
 // f.e. 042cb1572ffbea5d = 20001 = info@digitalvervede
 // 97e629a6dae69903 = rdannhaeuser
 // 4fdd2c0a1b6d0a7f = testuser von rdannhaeuser
-window.aoid = "97e629a6dae69903";
-window.uid = "97e629a6dae69903";
+window.aoid = "";
+window.uid = "";
